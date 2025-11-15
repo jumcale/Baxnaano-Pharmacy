aef9ccf22fafa32153845bd69595cb7da6e64198
Jimcale Abdirahman <149503934+jumcale@users.noreply.github.com>
2025-11-15 22:43:39 +0300

    Delete frontend directory

--- frontend/src/services/authService.ts
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-import { API_BASE_URL } from "../utils/apiClient";
-
-export const login = async (payload: { email: string; password: string }) => {
-  return axios.post(`${API_BASE_URL}/auth/login/`, payload);
-};
